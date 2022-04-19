7a0043ae0116262ec42d01dfa2876e13445a53e0
Celina Calma <ccalma@Celinas-MBP.attlocal.net>
2022-04-18 21:35:10 -0400

    removed patients endpoint

--- src/home-page/HomePageService.js
+++ src/home-page/HomePageService.js
@@ -5,19 +5,19 @@ import HttpHelper from '../../utils/HttpHelper';
  * @name getAllLanguages
  * @description Utilizes HttpHelper to make a GET request to an API
  * Gets all languages
- * @param {*} setLanguages sets state for languages
+ * @param {*} setAllLanguages sets state for languages
  * @param {*} setApiError sets error if response other than 200 is returned
  * @returns sets state for languages if 200 response, else sets state for apiError
  */
-export async function getAllLanguages(setLanguages, setApiError) {
-  await HttpHelper(Constants.PATIENTS_ENDPOINT, 'GET')
+export async function getAllLanguages(setAllLanguages, setApiError) {
+  await HttpHelper(Constants.WORMSTIES_ENDPOINT, 'GET')
     .then((response) => {
       if (response.ok) {
         return response.json();
       }
       throw new Error(Constants.API_ERROR);
     })
-    .then(setLanguages)
+    .then(setAllLanguages)
     .catch(() => {
       setApiError(true);
     });
