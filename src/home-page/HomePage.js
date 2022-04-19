1d4217c192f7c64d5904442789c0cc5695b5a597
Celina Calma <ccalma@Celinas-MBP.attlocal.net>
2022-04-18 20:59:57 -0400

    some boilerplate. trouble with react router

---
+++ src/home-page/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * @name EditEncounterPage
+ * @description Displays a form to edit an existing encounter
+ * @return component
+ */
+const HomePage = () => {
+console.log('wooo');
+  return (
+    <h1>oof</h1>
+  );
+};
+export default HomePage;
